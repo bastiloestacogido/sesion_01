0564b76572a2d650ae2b1fc3e066a64c57c8f619
Bastián Lobos <101819240+bastiloestacogido@users.noreply.github.com>
2023-04-15 09:13:50 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
 function setup() {
   createCanvas(windowWidth, windowHeight).position(0, 0).style("z-index", -1);
-  createA("https://…", "Mi cuenta de GitHub", "_blank").id("este");
+  createA("https://github.com/bastiloestacogido", "Mi cuenta de GitHub", "_blank").id("este");
   background(0);
 }
 
-function draw() {}
+function draw() {
+  textAlign(CENTER, CENTER);
+  textSize(25);
+  text("🐺", mouseX, mouseY);
+  
+}
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
